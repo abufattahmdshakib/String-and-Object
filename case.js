28accb11cf9b8e3fde19dc1291d8db19a9c4175e
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-10 13:22:47 +0600

    lowercase and upercase

---
+++ case.js
@@ -0,0 +1,12 @@
+// upercase and lowercase
+const school = 'nazipur gvet. model high school';
+console.log(school);
+const subject = '   Mate';
+const book = 'mate   ';
+if (subject.toLowerCase === book.toLowerCase) {
+    console.log('math bujhe korte parle onnek moja')
+
+}
+else {
+    console.log('math onnk hard amar mathar upor diye jai')
+}
