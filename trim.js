b831c9c65373903f5526d46e972069a257a78fd0
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-10 13:34:17 +0600

    trim used kore smner pichoner whaite spash valu less kora hoyeche

---
+++ trim.js
@@ -0,0 +1,8 @@
+const english = 'water  ';
+const bangla = '  water';
+if ( english.trim === bangla.trim ){
+    console.log( 'suddho panir opor nam jibon');
+}
+else{
+    console.log('osuddh pani mrttur karon');
+}
